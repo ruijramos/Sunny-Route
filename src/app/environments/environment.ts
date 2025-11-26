c77cf48b4461ce0c79860783082ed9527a75191a
Rui Ramos <ruiramos2604@gmail.com>
2025-11-26 14:29:13 +0000

    docs: Update README with app explanation and screenshots

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
 
-    teleport_api_search_url: "https://api.teleport.org/api/cities/?search=",
     nominatim_api_autocomplete_url: "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=",
     openweathermap_api_url: "https://api.openweathermap.org/data/2.5/forecast",
     osrm_api_url: "https://router.project-osrm.org/route/v1/driving/",
